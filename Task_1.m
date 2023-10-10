21cd91d86964e8afdede329d0d6dfcc28b5ad7da
Mohamed Ismail <fouadmohamed370@yahoo.com>
2023-10-10 20:09:33 +0200

    Add files via upload

---
+++ Task_1.m
@@ -0,0 +1,22 @@
+%Numeric Types
+intVar = 10;
+doubleVar = 25.25;
+disp(intVar)
+disp(doubleVar)
+
+
+%Creating Numeric Arrays
+evenNumbers = [2, 4, 6, 8,10];
+primeNumbers = [2; 3; 5; 7; 11];
+disp(evenNumbers)
+disp(primeNumbers)
+
+%Specialized Matrix Functions
+IM = eye(3,3);
+magicSquare = magicsquare(2);
+disp(IM)
+disp(magicSquare)
+
+
+
+
